84d306cce2b651e1e8d9b76161ece4dc947b90a1
Kadim Baldivieso Jauregui <a2020211131@estudiantes.upsa.edu.bo>
2021-10-13 17:53:20 -0400

    Creacion clase naipe

---
+++ SegundoParcial/Models/Naipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public string NaipeId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+
+        [Url]
+        public string Imagen { get; set; }
+        
+
+    }
+}
